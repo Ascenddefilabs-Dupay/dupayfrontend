cc66ed24e5c1f780c46dd80b42394a76e9ec0495
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2024-08-30 17:25:35 +0530

    created frontend fetching details and added filter

---
+++ src/app/TransactionHistory/page.jsx
@@ -0,0 +1,10 @@
+// TransactionHistory/page.jsx
+import UserTransaction from './UserTransaction';
+
+export default function Home() {
+  return (
+    <div>
+      <UserTransaction />
+    </div>
+  );
+}
