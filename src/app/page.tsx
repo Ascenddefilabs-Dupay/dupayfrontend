76b8b4dc8a6f88b580392d04cec66643808872c7
tej-2206 <sasi.venkat@ascenddefi.com>
2024-09-02 11:07:22 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import Navbar from './Userauthentication/LandingPage/Navbar';
+import Index from './Userauthentication/LandingPage';
 
 
 function Home({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <Index />
+      
       
       
       
