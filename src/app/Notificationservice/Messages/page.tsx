a56dfe743f670f121a6ab6f13590a24fa732d04a
DinakarY7 <dinakar.yaramati@ascenddefi.com>
2024-09-10 16:08:09 +0530

    Made changes in the push notifications

---
+++ src/app/Notificationservice/Messages/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Messages from './Messages';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <Messages />
+    </div>
+  );
+}
+
+export default Page;
