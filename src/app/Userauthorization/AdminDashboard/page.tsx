f96b6408fd47cf3d02ea61319e6af8bf60fb6202
VikramMoger1 <Vikram.Moger@ascenddefi.com>
2024-09-12 11:07:51 +0530

    added admindashboard

---
+++ src/app/Userauthorization/AdminDashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import AdminDahboard from "./AdminDashboard";
+
+const Dahboard:React.FC = () => {
+  return (
+    <div>
+      <AdminDahboard />
+    </div>
+  );
+};
+
+export default Dahboard;
