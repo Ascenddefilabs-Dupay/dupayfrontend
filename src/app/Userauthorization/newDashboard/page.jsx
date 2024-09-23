161d33e7a5878148a410c8783da018bffe1fea95
Sinu2000 <sinusrinivas861@gmail.com>
2024-09-23 16:53:04 +0530

    Made changes in new Dashboard in Ui

---
+++ src/app/Userauthorization/newDashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewDashboard from './newdashboard'
+
+const newdashboard = () => {
+  return (
+    <div>
+        <NewDashboard />
+    </div>
+  )
+}
+
+export default newdashboard
