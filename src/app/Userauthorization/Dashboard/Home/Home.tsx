0710bb5f262cb59687841cc929104b49ed6fcfd5
shivaprasadGow <Shivaprasad.G@ascenddefi.com>
2024-10-16 18:03:26 +0530

    'dashbord'

--- src/app/Userauthorization/Dashboard/Home/Home.tsx
+++ src/app/Userauthorization/Dashboard/Home/Home.tsx
@@ -112,6 +112,7 @@ const Home = () => {
 
   const [balance, setBalance] = useState<string | null>(null); // Allow string or null
   const [suiAddress, setSuiAddress] = useState<string | null>(null); // Allow string or null
+  const [showSuccessPage, setShowSuccessPage] = useState(false);
 
   
   useEffect(() => {
@@ -223,11 +224,15 @@ const Home = () => {
   
       try {
         const response = await axios.post("https://fiatmanagement-ind-255574993735.asia-south1.run.app/fiatmanagementapi/fiat_wallets/", payload);
-        
+        setShowSuccessPage(true);
         setAlertMessage("Wallet created successfully!");
+        resetForm();
         setAddNewFiatWalletPage(false);
         setAddNewFiatWallet(false);
-        setFiatDropdownVisible(true);
+        setTimeout(() => {
+          setShowSuccessPage(false);  // Hide success page
+          setFiatDropdownVisible(true); // Show Fiat dropdown
+        }, 5000);
         resetForm();
         
         // setFiatDropdownVisible(true);
@@ -1087,6 +1092,57 @@ const handleRefresh =()=>{
         </div>
         </div>
       )}
+      {showSuccessPage&& (
+        <div className={styles.backdrop}>
+          <div className={styles.modalContentSuccess}>
+            <div className={styles.successPage}>
+              <h2>Success</h2>
+              <div className={styles.successContent}>
+              
+              <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg" className="custom-wallet-icon">
+              <g clip-path="url(#clip0_1532_9597)">
+              <path d="M66.3798 52.4995C65.5518 52.5035 64.8838 53.1755 64.8838 54.0035C64.8838 54.8315 65.5558 55.5035 66.3838 55.4995C67.2118 55.4995 67.8838 54.8275 67.8838 53.9995C67.8838 53.1715 67.2118 52.4995 66.3798 52.4995" stroke="url(#paint0_linear_1532_9597)" stroke-width="3.625" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12.001 19.9995V73.9995C12.001 78.4195 15.581 81.9995 20.001 81.9995H76.001C80.421 81.9995 84.001 78.4195 84.001 73.9995V33.9995C84.001 29.5795 80.421 25.9995 76.001 25.9995H18.001C14.689 25.9995 12.001 23.3115 12.001 19.9995V19.9995C12.001 16.6875 14.689 13.9995 18.001 13.9995H68.001" stroke="url(#paint1_linear_1532_9597)" stroke-width="3.625" stroke-linecap="round" stroke-linejoin="round"/>
+              </g>
+              <defs>
+              <linearGradient id="paint0_linear_1532_9597" x1="64.3838" y1="51.9995" x2="68.4696" y2="52.0892" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#45F0D1"/>
+              <stop offset="1" stop-color="#76E268"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_1532_9597" x1="12.001" y1="13.9995" x2="85.5415" y2="15.7085" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#45F0D1"/>
+              <stop offset="1" stop-color="#76E268"/>
+              </linearGradient>
+              <clipPath id="clip0_1532_9597">
+              <rect width="96" height="96" fill="white"/>
+              </clipPath>
+              </defs>
+              </svg>
+              
+
+                <p className={styles.successMessage}>
+                  Your transaction was completed successfully!
+                </p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <button
+                  className={styles.goToDashboardButton}
+                  
+                >
+                  Go to Dashboard
+                </button>
+                <button
+                  className={styles.continueButton}
+                  
+                >
+                  Continue
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
 
       </div>
     </div>
